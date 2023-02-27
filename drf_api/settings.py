63063ac1d28247d70bc0e4d5321044228090c6fc
vivignutz <vgnutzmann@gmail.com>
2023-02-27 06:07:01 +0000

    Add model Profile

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -19,7 +19,7 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
-MEDIA_URL = '/media/
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles'
 ]
 
 MIDDLEWARE = [
