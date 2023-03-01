bd50f63ee9f51f2467de45a0ac8d96da915436cb
vivignutz <vgnutzmann@gmail.com>
2023-03-01 11:28:24 +0000

    Add profiles detail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,11 +3,14 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username') #nao pode ser editado
+    owner = serializers.ReadOnlyField(
+        source='owner.username')
+    # nao pode ser editado
 
-    class Meta: #aponta para o profile e especifica os campos que queremos incluir
+    class Meta:
+        # aponta para o profile e especifica os campos que queremos incluir
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image'
+            'id', 'owner', 'created_at', 'updated_at',
+            'name', 'content', 'image'
             ]       # ou coloca isso pra inserir tudo: '__all__'
