6e03890c45813b81aeb5fd2e21b48484acd858f1
vivignutz <vgnutzmann@gmail.com>
2023-03-02 11:10:59 +0000

    Add Postlist View tests

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
