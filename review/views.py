0ec559d187b74e33450d2716cf87bee4ddeb1d7c
vivignutz <vgnutzmann@gmail.com>
2023-03-07 11:46:05 +0000

    Add changes in review views.py

--- review/views.py
+++ review/views.py
@@ -15,4 +15,5 @@ class RatingView(generics.CreateAPIView):
 
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_201_CREATED,
+                        headers=headers)
