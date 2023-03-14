dbcd49b608867b4fd493cdac37d2909f82f10859
vivignutz <vgnutzmann@gmail.com>
2023-03-14 12:11:16 +0000

    Setted api.urls.py

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
