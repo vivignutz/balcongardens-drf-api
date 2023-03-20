2e64153f05bfe34e1a653fb6f3f69ff9af6e4de2
vivignutz <vgnutzmann@gmail.com>
2023-03-20 12:03:50 +0000

    Solved typo at settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,7 +48,7 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKE = 'my_app_auth'
+JWT_AUTH_COOKIE = 'my_app_auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
