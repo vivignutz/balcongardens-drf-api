aecd70d306cefaa9300dfc7141da42f8fda796ea
vivignutz <vgnutzmann@gmail.com>
2023-03-20 07:33:21 +0000

    Solved typo

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.get.env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEBUG' in os.environ
