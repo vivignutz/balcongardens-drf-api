ecbd23836b218dde707cc4c7ec5df9937944b97c
vivignutz <vgnutzmann@gmail.com>
2023-03-21 09:41:16 +0000

    Pushing

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,8 +62,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEBUG' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', 'balcongardens.herokuapp.com']
 
