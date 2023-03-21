5f64b2f1138e0c903be4738e8dd682c11a9c4dbf
vivignutz <vgnutzmann@gmail.com>
2023-03-21 08:49:17 +0000

    Testing DRF API token

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -33,8 +33,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-        )],
-
+    )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
